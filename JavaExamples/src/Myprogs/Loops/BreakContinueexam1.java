89d5e1b36807941a0567d4c2096c88c3cf718d0e
puneeth <puneethhp03@gmail.com>
2023-11-29 11:54:58 +0530

    "LoopsExam3 commit"

---
+++ JavaExamples/src/Myprogs/Loops/BreakContinueexam1.java
@@ -0,0 +1,16 @@
+package Myprogs.Loops;
+
+public class BreakContinueexam1 {
+        public static void main(String[] args) {
+
+            for(int i=1;i<=10;i++){
+                if(i==9){
+
+                    break;
+                }
+                System.out.println(i);
+            }
+        }
+    }
+
+
---
+++ JavaExamples/src/Myprogs/Loops/BreakContinueexam5.java
@@ -0,0 +1,17 @@
+package Myprogs.Loops;
+
+public class BreakContinueexam5 {
+    public static void main(String[] args) {
+        xy:
+        for (int i = 1; i <= 5; i++) {
+            pq:
+            for (int j = 1; j <= 5; j++) {
+                if (i == 2 && j == 2) {
+                    continue xy;
+                }
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+}
+
