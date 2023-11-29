0eb588a383180f10b860716ca422283dcbeb64ee
puneeth <puneethhp03@gmail.com>
2023-11-29 11:52:48 +0530

    "LoopsExam3 commit"

---
+++ JavaExamples/src/Myprogs/Loops/LoopsExam4.java
@@ -0,0 +1,15 @@
+package Myprogs.Loops;
+
+public class LoopsExam4 {
+        public static void main(String[] args)
+        {
+            int i=1;
+            while(i<=20)
+            {
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
+
+
