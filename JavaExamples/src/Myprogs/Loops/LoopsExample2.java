fc7580380ccefd5b7b8aef5afa82e95574fd7f67
puneeth <puneethhp03@gmail.com>
2023-11-30 11:03:59 +0530

    "LoopExample1 commit"

---
+++ JavaExamples/src/Myprogs/Loops/LoopsExample2.java
@@ -0,0 +1,13 @@
+package Myprogs.Loops;
+
+public class LoopsExample2 {
+        public static void main(String[] args)
+        {
+            int n, i;
+            n=2;
+            for(i=1;i<=10;i++)
+            {
+                System.out.println(n+"*"+i+"="+n*i);
+            }
+        }
+    }
