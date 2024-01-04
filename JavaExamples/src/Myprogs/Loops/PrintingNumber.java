83eb3f01379d9838e29fbfdd95245553f4e10e07
puneeth <puneethhp03@gmail.com>
2024-01-04 12:00:48 +0530

    "Loops_Examples"

---
+++ JavaExamples/src/Myprogs/Loops/PrintingNumber.java
@@ -0,0 +1,25 @@
+package Myprogs.Loops;
+
+public class PrintingNumber {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            switch (i) {
+                case 2:
+                    System.out.println(i);
+                    break;
+                case 3:
+                    System.out.println(i);
+                    break;
+                case 4:
+                    System.out.println(i);
+                    break;
+                case 5:
+                    System.out.println(i);
+                    break;
+                default:
+                    System.out.println("number not available");
+                    break;
+            }
+        }
+    }
+}
